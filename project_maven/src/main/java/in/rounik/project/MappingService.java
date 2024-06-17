1677b9e47482b9886cd8586abf32726d580af918
Rounik Roushan <126459205+Rounik19@users.noreply.github.com>
2024-06-17 13:22:03 +0530

    Update MappingService.java

--- project_maven/src/main/java/in/rounik/project/MappingService.java
+++ project_maven/src/main/java/in/rounik/project/MappingService.java
@@ -22,9 +22,9 @@ public class MappingService {
     private GroovyShell groovyShell;
     private File file;
     private Script script;
-    
+
     public MappingService(){
-        this.file = new File("src/main/resources/NestedField.groovy");
+        this.file = new File("src/main/java/in/rounik/project/MappingControllerScript.groovy");
         this.groovyShell = new GroovyShell();
         this.script = null;
         try{
@@ -32,20 +32,5 @@ public class MappingService {
         }catch(IOException e){
             e.printStackTrace();
         }
-    }
-    public JsonNode getNestedField(JsonNode json, String Path){
-        Object object = script.invokeMethod("getNestedField", new Object[] {json,Path});
-        return (JsonNode)object;
-    }
-
-    public JsonNode swapFields(JsonNode json, Map<String,Object> map){
-        // return (JsonNode) script.invokeMethod("swapFields", new Object[] {json.toString(),map});
-        return null;
-    }
-    public JsonNode mapData(JsonNode node, JsonNode dataMap){
-        return (JsonNode) script.invokeMethod("mapData", new Object[] {node, dataMap});
-    }
-    public JsonNode mapFields(JsonNode json, Map<String,String> fieldMap){
-        return (JsonNode) script.invokeMethod("mapFields", new Object[] {json, fieldMap});
     }
 }
