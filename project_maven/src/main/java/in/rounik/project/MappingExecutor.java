b92ca8c90d0782dae09b64ffe08e1fff0a4cdca8
Rounik Roushan <126459205+Rounik19@users.noreply.github.com>
2024-06-17 18:22:19 +0530

    Update MappingExecutor.java

--- project_maven/src/main/java/in/rounik/project/MappingExecutor.java
+++ project_maven/src/main/java/in/rounik/project/MappingExecutor.java
@@ -3,6 +3,7 @@ package in.rounik.project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +12,10 @@ public class MappingExecutor {
     @Autowired
     private MappingService mappingService;
 
-    @GetMapping("/executeMapping")
-    public ResponseEntity<String> executeTransformation() {
-        mappingService.executeDataTransformationScript();
-        return ResponseEntity.ok("Mapping script executed successfully.");
+    @GetMapping("/executeMapping/{id}")
+    public ResponseEntity<?> executeTransformation(@PathVariable String id){
+        Object result = mappingService.executeDataTransformationScript(id);
+        return ResponseEntity.ok().body(result);
     }
     
 }
